410698b338d7149bd96a3f5734b86f759e54792d
Smith <andrew.smith@swissborg.com>
2024-12-29 10:13:35 +0000

    day 10 - done

--- 2024/common/src/coords.rs
+++ 2024/common/src/coords.rs
@@ -1,5 +1,5 @@
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug, Eq, Hash, PartialEq)]
 pub struct Coords {
     pub x: i32,
     pub y: i32
--- 2024/day_10/src/main.rs
+++ 2024/day_10/src/main.rs
@@ -1,14 +1,13 @@
-use itertools::Itertools;
+use std::collections::HashSet;
 use common::coords::Coords;
 use common::grid::Grid;
 use common::reader::read_grid_default;
 
-
-fn traverse_path(input: &Grid<i32>, coords: &Coords) -> i32 {
-    fn inner(input: &Grid<i32>, coords: Coords, search_val: i32) -> i32 {
+fn traverse_path<'a>(input: &Grid<i32>, coords: &Coords) -> Vec<Coords> {
+    fn inner(input: &Grid<i32>, coords: Coords, search_val: i32) -> Vec<Coords> {
         Coords::CARDINALS
             .iter()
-            .fold(0, |acc, Coords {x: cardinal_col, y: cardinal_row }| {
+            .fold(vec![], |mut acc, Coords {x: cardinal_col, y: cardinal_row }| {
                 let new_row = coords.y + cardinal_row;
                 let new_col = coords.x + cardinal_col;
                 let x = *input
@@ -19,10 +18,11 @@ fn traverse_path(input: &Grid<i32>, coords: &Coords) -> i32 {
                     .unwrap_or(&-1);
 
                 if x == 9 && search_val == 9 {
-                    println!("head at {:?}, {:?}, acc: {:?}", new_col, new_row, acc);
-                    1
+                    acc.push(Coords{x: new_col, y: new_row });
+                    acc
                 } else if x == search_val {
-                    acc + inner(input, Coords{x: new_col, y: new_row }, search_val + 1)
+                    acc.extend(inner(input, Coords{x: new_col, y: new_row }, search_val + 1));
+                    acc
                 } else {
                     acc
                 }
@@ -31,18 +31,30 @@ fn traverse_path(input: &Grid<i32>, coords: &Coords) -> i32 {
     inner(input, *coords,1)
 }
 
-fn traverse_paths(input: &Grid<i32>, starts: &Vec<Coords>) -> i32 {
+fn traverse_paths_distinct_heads(input: &Grid<i32>, starts: &Vec<Coords>) -> i32 {
     starts
         .iter()
         .fold(0, |acc, coords|{
-            acc + traverse_path(input, coords)
+            let heads: HashSet<Coords> = HashSet::from_iter(traverse_path(input, coords));
+            acc + heads.iter().count() as i32
         })
-
 }
 
+fn traverse_paths_distinct_paths(input: &Grid<i32>, starts: &Vec<Coords>) -> i32 {
+    starts
+        .iter()
+        .fold(0, |acc, coords|{
+            let heads = traverse_path(input, coords);
+            acc + heads.iter().count() as i32
+        })
+}
+
+
 fn main() {
-    let input = &read_grid_default("./resources/test.csv").expect("Successful input read");
+    let input = &read_grid_default("./resources/input.csv").expect("Successful input read");
     let starts = input.find(0);
-    let part_1 = traverse_paths(input, &starts);
-    println!("{:?}",part_1);
+    let part_1 = traverse_paths_distinct_heads(input, &starts);
+    println!("Part 1: {:?}",part_1);
+    let part_2 = traverse_paths_distinct_paths(input, &starts);
+    println!("Part 2: {:?}",part_2);
 }
